45bbca04bcf3c9f47c27ef5568d7761b8d1c82c0
LarysaTymoshenko <luda76@ukr.net>
2021-11-28 22:35:56 +0200

    2235

--- src/component/friends/FiendsList.js
+++ src/component/friends/FiendsList.js
@@ -1,14 +1,32 @@
 import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
-export default function FriendList({ id, avatar, name, isOnline }) {
+export default function FriendList({ friends }) {
   return (
-    <ul className="friend-list">
-      <li className="item">
-        <span className="status">{isOnline}</span>
-        <img className={avatar} src="" alt={name} width="48" />
-        <p className="name">{name}</p>
-      </li>
+    <ul className={s.list}>
+      {friends.map(friend => (
+        <li key={friend.id} className={s.item}>
+          <span
+            className={s.status}
+            style={{ backgroundColor: friend.isOnline ? 'green' : 'red' }}
+          >
+            {friend.isOnline ? 'online' : 'offline'}
+          </span>
+          <img
+            className={s.avatar}
+            src={friend.avatar}
+            alt={friend.name}
+            width="48"
+          />
+          <p className={s.name}>{friend.name}</p>
+        </li>
+      ))}
     </ul>
   );
 }
+
+FriendList.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
